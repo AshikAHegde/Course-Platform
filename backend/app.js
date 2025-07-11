9a62e416d6af37aa0142afa2550e10b6638db8c9
AshikAHegde <hegdeashik12@gmail.com>
2025-07-11 23:21:00 +0530

    minor

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const app = express();
-
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT ;
 const jwb = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const connectDB = require('./DataBase/db');
-const authRoutes = require('./Routes/auth/signup');
+const authRoutes = require('./routes/auth');
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
-app.use('auth' , authRoutes )
+app.use('/auth' , authRoutes )
 
 app.listen(PORT, () => {
     connectDB();
