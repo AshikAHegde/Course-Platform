18b38f534924d15be626b8468ddb261cd7ebb180
AshikAHegde <hegdeashik12@gmail.com>
2025-07-11 19:06:01 +0530

    add middleware for auth

---
+++ backend/middleware/validateSignup.js
@@ -0,0 +1,13 @@
+const { body } = require('express-validator');
+
+const validateSignup = [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+];
+const validateLogin = [
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+];
+
+module.exports = { validateSignup , validateLogin };
