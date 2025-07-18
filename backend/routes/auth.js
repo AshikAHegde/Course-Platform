23d90edd2ab37d15fed48c362e49c3a676ad3a48
AshikAHegde <hegdeashik12@gmail.com>
2025-07-18 21:56:28 +0530

    corrected auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,9 +5,10 @@ const login = require('../controllers/login');
 const { validateSignup , validateLogin } = require('../middleware/validateSignup');
 const emailsend = require('../middleware/sendmail');
 const emailVerifyController = require('../controllers/emailverify');
+const logout = require('../controllers/logout');
 
 router.post('/signup', validateSignup, signup);
-router.post('/login', validateLogin, login);
+router.post('/login', validateLogin, login); 
 router.post('/emailsend', emailsend);  //having query parameter token
 router.post('/emailverify', emailVerifyController);  
 
