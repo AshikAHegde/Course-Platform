0326741cdd9c4d5b07c0411d1634b7e57299fa08
AshikAHegde <hegdeashik12@gmail.com>
2025-07-22 19:47:04 +0530

    some changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,7 +5,6 @@ const login = require('../controllers/login');
 const { validateSignup , validateLogin } = require('../middleware/validateSignup');
 const emailsend = require('../middleware/sendmail');
 const emailVerifyController = require('../controllers/emailverify');
-const logout = require('../controllers/logout');
 
 router.post('/signup', validateSignup, signup);
 router.post('/login', validateLogin, login); 
