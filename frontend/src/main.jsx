f26d65ea584b5b042e37198a39fd1b9fbdb210bb
Shivam <snoozing44@gmail.com>
2025-07-22 17:13:12 +0530

    Added Sign_In and Sign_Up page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Email_Verfication_Page from './components/Email_Verfication_Page.jsx'
+import App from './App.jsx' // Make sure App.jsx is in the same folder
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Email_Verfication_Page/>
+    <App />
   </StrictMode>,
 )
