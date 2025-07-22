1d9bb54df059f6de279b25fbf4b225e8fa3bcac4
Shivam <snoozing44@gmail.com>
2025-07-22 16:22:39 +0530

    added Email_Verfication_Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Email_Verfication_Page from './components/Email_Verfication_Page.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Email_Verfication_Page/>
   </StrictMode>,
 )
